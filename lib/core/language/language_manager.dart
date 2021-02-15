2fa01ae38a81270a223a2777c35252d87ee480e5
enesekinci <enes.eknc.96@gmail.com>
2021-02-15 17:39:03 +0300

    basic language manager created

---
+++ lib/core/language/language_manager.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LanguageManager extends Translations {
+  static LanguageManager _instance = LanguageManager._init();
+  static LanguageManager get instance => _instance;
+
+  LanguageManager._init();
+
+  final locale = Locale('tr', 'TR');
+  final enLocale = Locale("en", "US");
+  final fallbackLocale = Locale('tr', 'TR');
+
+  final langs = [
+    'English',
+    'Türkçe',
+    '日本語',
+  ];
+
+  final locales = [
+    Locale('en', 'US'),
+    Locale('tr', 'TR'),
+    Locale('ja', 'JP'),
+  ];
+
+  void changeLocale(String lang) {
+    final locale = _getLocaleFromLanguage(lang);
+    Get.updateLocale(locale);
+  }
+
+  Locale _getLocaleFromLanguage(String lang) {
+    for (int i = 0; i < langs.length; i++) {
+      if (lang == langs[i]) return locales[i];
+    }
+    return Get.locale;
+  }
+
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en_US': enUS,
+        'tr_TR': trTR,
+        'ja_JP': jaJP,
+      };
+
+  // English Translations
+  final Map<String, String> enUS = {
+    'hello': 'Hello!',
+  };
+
+  // Japanese Translations
+  final Map<String, String> jaJP = {
+    'hello': '今日は!',
+  };
+
+  // Turkish Translations
+  final Map<String, String> trTR = {
+    'hello': 'Merhaba!',
+  };
+}
